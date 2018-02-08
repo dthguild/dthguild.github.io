3525d0d334e3de5811e7bae24b8c7a87262f6b44
dthguild <36249800+dthguild@users.noreply.github.com>
2018-02-07 20:53:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-$("input[name='calc']").next().click(function() {
-  alert( "Handler for .click() called." );
+$(document).ready(function(){
+  $("input[name='calc']").click(function() {
+    var might  = parseInt($("input[name='might']").val());
+    var kills  = parseInt($("input[name='kills']").val());
+    var stone  = parseInt($("input[name='stone']").val());
+    var timber = parseInt($("input[name='timber']").val());
+    var ore    = parseInt($("input[name='ore']").val());
+    var helps  = parseInt($("input[name='helps']").val());
+    
+    var rss = stone + timber + ore;
+    
+    var res = might/5e6 + kills/1e6 + rss/25e6 + Math.max(0,(helps-5000)/5000);
+    
+    res = parseFloat(Math.round(res * 100) / 100).toFixed(2);
+    
+    res = "Your points: " + res
+    
+    $("#result").text(res);
+  });
 });
